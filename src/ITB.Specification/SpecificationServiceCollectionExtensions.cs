668839913c42ebbbd74586221345fd66c846bc46
Denis Novogrodsky <denis.novogrodsky@ids-group.co.uk>
2020-04-29 18:39:23 +0300

    add specification factory

---
+++ src/ITB.Specification/SpecificationServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace ITB.Specification
+{
+    public static class SpecificationServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSpecificationFactory(this IServiceCollection services)
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+
+            services.TryAddTransient<ISpecificationFactory, SpecificationFactory>();
+
+            return services;
+        }
+    }
+}
